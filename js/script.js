941e4718b48d7b8779f98834dad21f6c4e90caea
Bhavya Grover <bgrover4@my.bcit.ca>
2022-11-08 15:39:27 -0800

    Creates the do while loop for the prompt

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
 let courselist=[{"ACIT 1620":"Fundamental Web Technology","ACIT 1515":"Scripting for IT","ACIT 1630":"Database Systems"}];
+let input;
+do{
+    input=(prompt("Enter a 4-digit number."));
+}while(isNaN(Number(input)) || input.length != 4 )
